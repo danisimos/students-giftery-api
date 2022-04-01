d41dfa4732677f74075e06b819260250943a4b1e
danisimos <danil.korotaev02@gmail.com>
2022-04-01 19:05:00 +0300

    added swagger ui

--- src/main/java/ru/itis/studentsgiftery/controllers/BrandsController.java
+++ src/main/java/ru/itis/studentsgiftery/controllers/BrandsController.java
@@ -1,4 +1,9 @@
 package ru.itis.studentsgiftery.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import ru.itis.api.BrandsApi;
+
 public class BrandsController {
+
 }
