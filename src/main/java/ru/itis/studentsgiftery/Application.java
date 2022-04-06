9a43ae9c54e489c80fc12cfa23c5f1bb4b13e8dd
danisimos <danil.korotaev02@gmail.com>
2022-04-06 12:09:12 +0300

    added spring security

--- src/main/java/ru/itis/studentsgiftery/Application.java
+++ src/main/java/ru/itis/studentsgiftery/Application.java
@@ -2,12 +2,24 @@ package ru.itis.studentsgiftery;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Application {
+    @Value("api.title")
+    private String apiTitle;
+
+    @Value("api.version")
+    private String apiVersion;
+
+    @Value("api.description")
+    private String apiDescription;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
@@ -15,8 +27,13 @@ public class Application {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI().info(new Info()
-                .title("Subscriptions service API")
-                .version("0.1")
-                .description("Service for POST subscriptions"));
+                .title(apiTitle)
+                .version(apiVersion)
+                .description(apiDescription));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
