548b1a1acdf25597b91dd59e9e17717dc7289bec
danisimos <danil.korotaev02@gmail.com>
2022-04-06 12:22:48 +0300

    updated models

--- src/main/java/ru/itis/studentsgiftery/controllers/BrandsController.java
+++ src/main/java/ru/itis/studentsgiftery/controllers/BrandsController.java
@@ -2,7 +2,6 @@ package ru.itis.studentsgiftery.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.itis.api.BrandsApi;
 import ru.itis.studentsgiftery.services.BrandsService;
