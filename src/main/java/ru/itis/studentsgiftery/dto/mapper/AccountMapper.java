8c1f263843141d281f212996ba9c9856b39125ba
DinisKhusaenov <dinis30094@gmail.com>
2022-04-07 16:00:28 +0300

    add List

--- src/main/java/ru/itis/studentsgiftery/dto/mapper/AccountMapper.java
+++ src/main/java/ru/itis/studentsgiftery/dto/mapper/AccountMapper.java
@@ -4,10 +4,16 @@ import org.mapstruct.Mapper;
 import ru.itis.studentsgiftery.dto.AccountDto;
 import ru.itis.studentsgiftery.models.Account;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
 
     Account toAccount(AccountDto accountDto);
 
     AccountDto toAccountDto(Account account);
+
+    List<Account> toAccountList(List<AccountDto> accountsDto);
+
+    List<AccountDto> toAccountDtoList(List<Account> accounts);
 }
